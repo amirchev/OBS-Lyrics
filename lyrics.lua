5e76bf9f72b3b9d9ec42aa1da1de8dd18a7ffa2c
DCStrato <76598794+DCStrato@users.noreply.github.com>
2021-09-27 20:31:02 -0600

    Update lyrics.lua

--- lyrics.lua
+++ lyrics.lua
@@ -2106,40 +2106,6 @@ function source_showing(cd)
     load_song(source, true)
 end
 
-function ParseCSVLine (line,sep) 
-	local res = {}
-	local pos = 1
-	sep = sep or ','
-	while true do 
-		local c = string.sub(line,pos,pos)
-		if (c == "") then break end
-		if (c == '"') then
-			local txt = ""
-			repeat
-				local startp,endp = string.find(line,'^%b""',pos)
-				txt = txt..string.sub(line,startp+1,endp-1)
-				pos = endp + 1
-				c = string.sub(line,pos,pos) 
-				if (c == '"') then txt = txt..'"' end 
-			until (c ~= '"')
-			table.insert(res,txt)
-			assert(c == sep or c == "")
-			pos = pos + 1
-		else	
-			local startp,endp = string.find(line,sep,pos)
-			if (startp) then 
-				table.insert(res,string.sub(line,pos,startp-1))
-				pos = endp + 1
-			else
-				table.insert(res,string.sub(line,pos))
-				break
-			end 
-		end
-	end
-	return res
-end
-
-
 function dbg(message)
     if DEBUG then
         print(message)
